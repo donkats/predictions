5af7557de7312733f1765018753064b9e3d79176
donkats <60600316+donkats@users.noreply.github.com>
2020-04-02 21:12:59 +0200

    made calculator for home away or draw result

--- client/src/App/pages/Predictions.js
+++ client/src/App/pages/Predictions.js
@@ -1,35 +1,58 @@
 import React, { Component } from 'react';
 import './Pages.css';
 
+const mockResult = 'away'
+
+function calculator(home, away) {
+  if (home < 0 || away < 0) {
+    return "error";
+  }
+  let result;
+  let userPoints = 0;
+  if (home > away) {
+    result = 'home';
+  }
+  if (away > home) {
+    result = 'away';
+  }
+  if (home === away) {
+    result = 'draw'
+  }
+  if (result === mockResult) {
+    userPoints + 5;
+  }
+  return userPoints;
+}
+
 class Predictions extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       name: '',
       value: '',
     }
   }
-  
+
   changeHandler = (event) => {
     this.setState({
-      name: event.target.name,
-      value: event.target.value,
+      homeGoals: event.target.homeGoals,
+      awayGoals: event.target.homeGoals,
     });
   }
 
-  render () {
+  render() {
     return (
       <div className="inputFields">
         <div>
-        <input type="text" value={this.state.name} onChange={this.changeHandler}/>
-        <div>{this.state.name}</div>
+          <input type="text" value={this.state.homeGoals} onChange={this.changeHandler} />
+          <div>{this.state.homeGoals}</div>
         </div>
         <div>
-        <input type="text" value={this.state.value} onChange={this.changeHandler}/>
-        <div>{this.state.value}</div>
+          <input type="text" value={this.state.awayGoals} onChange={this.changeHandler} />
+          <div>{this.state.awayGoals}</div>
         </div>
       </div>
-    );        
+    );
   }
 }
 
